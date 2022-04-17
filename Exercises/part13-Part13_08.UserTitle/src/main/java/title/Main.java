7ffbc717d89beee539e25fdec0cb6ae8c0408179
MHWillard <mhwillard@gmail.com>
2022-04-17 19:21:00 -0400

    .

---
+++ Exercises/part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -0,0 +1,17 @@
+package title;
+
+
+public class Main {
+
+    //user interface
+    //get name title
+    //new Title object(name, title)
+    //get name, get title
+    //add to params
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+
+}
---
+++ Exercises/part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -0,0 +1,21 @@
+package title;
+
+
+public class UserTitle {
+    private String name;
+    private String title;
+    
+    public UserTitle(String name, String title) {
+        this.name = name;
+        this.title = title;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+}
